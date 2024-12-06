98e063f43a10561c5b81b770fa0cbe8ac4ce77fa
Eri Maulana <maulanabouble11@gmail.com>
2024-12-06 10:52:18 +0700

    Mengubah jumlah stok setelah proses pesanan dibuat

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+use App\Observers\OrderProductObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+ 
+#[ObservedBy([OrderProductObserver::class])]
 class OrderProduct extends Model
 {
     use HasFactory;
