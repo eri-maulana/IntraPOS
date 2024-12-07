63a17f5dff16ed5e13565bc216cc5724b473432a
Eri Maulana <maulanabouble11@gmail.com>
2024-12-07 11:26:50 +0700

    membuat fitur keranjang belanja

--- app/Livewire/Pos.php
+++ app/Livewire/Pos.php
@@ -20,15 +20,17 @@ class Pos extends Component implements HasForms
 {
     use InteractsWithForms;
     public $search = '';
-    public $nameCustomer = '';
-    public $paymentMethods;
+    public $name_customer = '';
+    public $payment_methods;
+    public $order_items = [];
+    public $total_price;
 
     public function render()
     {
         return view('livewire.pos', [
             'products' => Product::where('stock', '>', 0)
-                                ->search($this->search)
-                                ->paginate(9)
+                ->search($this->search)
+                ->paginate(9)
         ]);
     }
 
@@ -41,7 +43,7 @@ class Pos extends Component implements HasForms
                         TextInput::make('nameCustomer')
                             ->required()
                             ->maxLength(255)
-                            ->default(fn () => $this->nameCustomer),
+                            ->default(fn() => $this->name_customer),
                         Select::make('gender')
                             ->options([
                                 'male' => 'Laki - laki',
@@ -51,14 +53,60 @@ class Pos extends Component implements HasForms
                         Select::make('payment_method_id')
                             ->required()
                             ->label('Metode Pemabayaran')
-                            ->options($this->paymentMethods->pluck('name', 'id'))
+                            ->options($this->payment_methods->pluck('name', 'id'))
                     ])
             ]);
     }
     public function mount()
     {
-        $this->paymentMethods = PaymentMethod::all();
-        $this->form->fill(['payment_methods', $this->paymentMethods]);
+        $this->payment_methods = PaymentMethod::all();
+        $this->form->fill(['payment_methods', $this->payment_methods]);
     }
 
+    public function addToOrder($productId)
+    {
+        $product = Product::find($productId);
+
+        if ($product) {
+            if ($product->stock <= 0) {
+                Notification::make()
+                    ->title('Stok Habis')
+                    ->danger()
+                    ->send();
+                return;
+            }
+
+            $existingItemKey = null;
+            foreach ($this->order_items as $key => $item) {
+                if ($item['product_id'] == $productId) {
+                    $existingItemKey = $key;
+                    break;
+                }
+            }
+
+            if($existingItemKey !== null){
+                $this->order_items[$existingItemKey]['quantity']++;
+            } else {
+                $this->order_items[] = [
+                    'product_id' => $product->id,
+                    'name' => $product->name,
+                    'price' => $product->price,
+                    'image_url' => $product->image_url,
+                    'quantity' => 1,
+                ];
+            }
+
+            session()->put('orderItems', $this->order_items);
+            Notification::make()
+                    ->title('Produk ditambahkan ke keranjang')
+                    ->success()
+                    ->send();
+        }
+    }
+
+    public function loadOrderItems($orderItems)
+    {
+        $this->order_items = $orderItems;
+        session()->put('orderItems', $orderItems);
+    }
 }
