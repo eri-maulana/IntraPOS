d8c7b9b572543e5fb5baf151ed4f4b46973fa53e
Eri Maulana <maulanabouble11@gmail.com>
2024-12-07 21:53:35 +0700

    membuat template untuk import produk

--- app/Exports/TemplateExport.php
+++ app/Exports/TemplateExport.php
@@ -3,15 +3,69 @@
 namespace App\Exports;
 
 use App\Models\Category;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
-class TemplateExport implements FromCollection
+class TemplateExport implements WithMultipleSheets
+{
+    public function sheets(): array
+    {
+        return [
+            new ProductsExport(),
+            new CategoriesExport(),
+        ];
+    }
+}
+
+class ProductsExport implements FromCollection, WithHeadings, WithTitle
+{
+    public function collection()
+    {
+        return collect([]);
+    }
+
+    public function headings(): array
+    {
+        return [
+            'nama produk',
+            'id_kategori',
+            'stok',
+            'harga',
+            'status aktif',
+            'gambar',
+            'barcode',
+            'deskripsi',
+        ];
+    }
+
+    public function title(): string
+    {
+        return 'Template Produk';
+    }
+}
+
+class CategoriesExport implements FromCollection, WithHeadings, WithTitle
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Category::all();
+        return Category::select('id', 'name')->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'id',
+            'nama kategori',
+        ];
+    }
+
+    public function title(): string
+    {
+        return 'Template Kategori';
     }
 }
