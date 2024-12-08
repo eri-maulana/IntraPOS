23fb58ae1ec0ea401ea120c0ea586f9e9ed64bbc
Eri Maulana <maulanabouble11@gmail.com>
2024-12-08 10:20:14 +0700

    menampilkan chart omset

---
+++ app/Filament/Widgets/OmsetChart.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Order;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class OmsetChart extends ChartWidget
+{
+    protected static ?string $heading = 'Omset';
+    protected static ?int $sort = 1;
+    public ?string $filter = 'today';
+
+    protected function getData(): array
+    {
+        $activeFilter = $this->filter;
+        $data = Trend::model(Order::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->sum('total_price');
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Omset per bulan',
+                    'data' => $data->map(fn(TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn(TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getFilters(): ?array
+    {
+        return [
+            'today' => 'Today',
+            'week' => 'Last week',
+            'month' => 'Last month',
+            'year' => 'This year',
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
