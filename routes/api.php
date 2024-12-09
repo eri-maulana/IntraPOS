fbb22e5fb27bf370457d882772860ed095db6ead
Eri Maulana <maulanabouble11@gmail.com>
2024-12-09 09:41:47 +0700

    Membuat API Payment Method

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PaymentMethodController;
 use App\Http\Controllers\Api\ProductController;
 
 Route::post('login', [AuthController::class, 'login']);
@@ -10,6 +11,7 @@ Route::post('login', [AuthController::class, 'login']);
 Route::apiResource('products', ProductController::class)->middleware(['auth:sanctum']);
 
 Route::get('products/barcode/{barcode}', [ProductController::class, 'showByBarcode'])->middleware(['auth:sanctum']);
+Route::get('payment-methods', [PaymentMethodController::class, 'index'])->middleware(['auth:sanctum']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
