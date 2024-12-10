9c995430a0ecb9883e394ba648283e56238f5b07
Eri Maulana <maulanabouble11@gmail.com>
2024-12-10 09:12:42 +0700

    Membuat widget table produk hampir habis

---
+++ app/Filament/Widgets/ProductAlert.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Tables;
+use App\Models\Product;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class ProductAlert extends BaseWidget
+{
+    protected static ?string $heading = 'Produk Hampir Habis';
+
+    protected static ?int $sort = 3;
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                Product::query()->where('stock', '<=', '10')->orderBy('stock', 'asc')
+            )
+            ->columns([
+                Tables\Columns\ImageColumn::make('image')
+                    ->label('Gambar'),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->label('Nama'),
+                Tables\Columns\BadgeColumn::make('stock')
+                    ->label('Stok')
+                    ->numeric()
+                    ->color(static function ($state): string {
+                        if($state < 5){
+                            return 'danger';
+                        } elseif ($state <= 10){
+                            return 'warning';
+                        }
+                    })
+                    ->sortable(),
+            ])
+            ->defaultPaginationPageOption(5);
+    }
+}
